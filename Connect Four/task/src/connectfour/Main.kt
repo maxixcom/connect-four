2bde0e3d7791648f005ec334113aa2d03e73dc5c
Maxim Savenko <maxim.savenko@gmail.com>
2021-10-04 11:44:35 +0300

    ...

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -1,60 +1,8 @@
 package connectfour
 
-import connectfour.console.extensions.print
-import connectfour.entity.Board
+import connectfour.console.ConsoleRunner
 
 fun main() {
-    printWelcomeMessage()
-    println("First player's name:")
-    val player1 = readLine()!!
-    println("Second player's name:")
-    val player2 = readLine()!!
-
-    var rows: Int = 6
-    var columns: Int = 7
-
-    while (true) {
-        println("Set the board dimensions (Rows x Columns)")
-        println("Press Enter for default (6 x 7)")
-        try {
-            readLine()?.let { input ->
-                if (input.isEmpty()) {
-                    return@let
-                }
-                val regex = "^\\s*(\\d+)\\s*x\\s*(\\d+)\\s*$".toRegex(RegexOption.IGNORE_CASE)
-                regex.matchEntire(input)?.let {
-                    rows = it.groups[1]!!.value.toInt()
-                    columns = it.groups[2]!!.value.toInt()
-                    if (rows !in (5..9)) {
-                        throw IllegalArgumentException("Board rows should be from 5 to 9")
-                    }
-                    if (columns !in (5..9)) {
-                        throw IllegalArgumentException("Board columns should be from 5 to 9")
-                    }
-                } ?: throw IllegalArgumentException("Invalid input")
-            }
-            break
-        } catch (e: Exception) {
-            println(e.message)
-        }
-    }
-
-    println("$player1 VS $player2")
-    println("$rows X $columns board")
-
-    val board = Board(
-        rows = rows,
-        columns = columns
-    )
-
-//    val board = List(rows) {
-//        MutableList(columns) {
-//            ' '
-//        }
-//    }
-    board.print()
-}
-
-fun printWelcomeMessage() {
-    println("Connect Four")
+    ConsoleRunner()
+        .run()
 }
--- Connect Four/task/src/connectfour/console/ConsoleRunner.kt
+++ Connect Four/task/src/connectfour/console/ConsoleRunner.kt
@@ -1,8 +1,31 @@
 package connectfour.console
 
+import connectfour.ConsoleApplication
+import connectfour.repository.GameRepository
+import connectfour.service.GameServiceFactory
+import connectfour.service.PlayerFactory
+
 class ConsoleRunner {
+    private val playerFactory: PlayerFactory = ConsoleApplication.playerFactory
+    private val gameServiceFactory: GameServiceFactory = ConsoleApplication.gameServiceFactory
+    private val gameRepository: GameRepository = ConsoleApplication.gameRepository
 
     fun run() {
-    }
+        printWelcomeMessage()
+        val (playerName1, playerName2) = readPlayerNames()
+        var (rows, columns) = readBoardsDimensions()
 
+        val idGame = gameRepository.create(
+            player1 = playerFactory.newPlayer(playerName1),
+            player2 = playerFactory.newPlayer(playerName1),
+            rows = rows,
+            columns = columns,
+        )
+
+        val game = gameRepository.findById(idGame) ?: throw Exception("Game not found exception")
+        val gameService = gameServiceFactory.newServiceForGame(game)
+
+        game.printStartMessage()
+        game.board.print()
+    }
 }
